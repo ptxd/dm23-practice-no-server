f105b68c2cacbb9335095c16cab8029c9dc88bc7
ptxd <ptxdas@gmail.com>
2017-05-28 14:51:33 -0600

    phase 3 partial completion

--- dist/js/bundle.js
+++ dist/js/bundle.js
@@ -29,4 +29,41 @@ angular.module('fin').directive('navDirective', function () {
         restrict: 'E'
     };
 });
+'use strict';
+
+angular.module('fin').run(['$anchorScroll', function ($anchorScroll) {
+            $anchorScroll.yOffset = 60; // always scroll by 50 extra pixels
+}]).controller('anchor', function ($scope, $location, $anchorScroll) {
+            $scope.how = function () {
+                        // set the location.hash to the id of
+                        // the element you wish to scroll to.
+                        $location.hash('how');
+                        // call $anchorScroll()
+                        $anchorScroll();
+            };
+
+            $scope.discover = function () {
+                        // set the location.hash to the id of
+                        // the element you wish to scroll to.
+                        $location.hash('discover');
+                        // call $anchorScroll()
+                        $anchorScroll();
+            };
+
+            $scope.contact = function () {
+                        // set the location.hash to the id of
+                        // the element you wish to scroll to.
+                        $location.hash('contact');
+                        // call $anchorScroll()
+                        $anchorScroll();
+            };
+
+            $scope.home = function () {
+                        // set the location.hash to the id of
+                        // the element you wish to scroll to.
+                        $location.hash('home');
+                        // call $anchorScroll()
+                        $anchorScroll();
+            };
+});
 //# sourceMappingURL=bundle.js.map
